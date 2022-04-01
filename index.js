f0abbbe22467a9a7810975a9c6eee7068722412f
fernando <ferho20u12@gmail.com>
2022-03-31 18:41:52 -0600

    alpha

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-    let map = L.map('map').setView([20.65856439281416, -103.32348510503071], 6);
 
     function geoloc() {
         d=document.getElementById("demo");
@@ -13,10 +12,11 @@
       function showPosition(position){
         d.innerHTML+="<p>Latitud: "+position.coords.latitude+"</p>";
         d.innerHTML+="<p>Longitud: "+position.coords.longitude+"</p>";
+        let map = L.map('map').setView([position.coords.latitude,position.coords.longitude], 3);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-        L.marker([20.65856439281416, -103.32348510503071]).addTo(map)
+        L.marker([position.coords.latitude,position.coords.longitude]).addTo(map)
             .bindPopup('Tu estas aqui')
             .openPopup();
       }
